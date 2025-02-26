0f8de78d8ed997b53edb7066357117da63b973ce
Cesar_Reyes_V <cesarreyesv2022@gmail.com>
2025-02-26 20:10:57 +0000

    Reyes Vazquez practicas inconos

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,68 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisIconosApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconosApp extends StatelessWidget {
+  const MisIconosApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+              title: Text("Cesar Reyes Vazquez "),
+              titleTextStyle: TextStyle(color: Colors.black, fontSize: 20),
+              centerTitle: true,
+              backgroundColor: Color(0xffff0000)),
+          body: Column(
+            children: <Widget>[
+              Text(
+                "Cesar Reyes Vazquez 22308051281294",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.blue),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  SizedBox(
+                    height: 100,
+                  ),
+                  Icon(
+                    Icons.face,
+                    color: Colors.pink,
+                    size: 24.0,
+                    semanticLabel: 'Text to announce in accessibility modes',
+                  ),
+                  Icon(
+                    Icons.abc_outlined,
+                    color: Colors.green,
+                    size: 30.0,
+                  ),
+                  Icon(
+                    Icons.label_important,
+                    color: Colors.blue,
+                    size: 36.0,
+                  ),
+                  Icon(
+                    Icons.h_mobiledata,
+                    color: Colors.green,
+                    size: 30.0,
+                  ),
+                  Icon(
+                    Icons.offline_bolt,
+                    color: Colors.green,
+                    size: 30.0,
+                  ),
+                ],
+              ),
+            ],
+          )),
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
-  }
-}
+  } //finwiggets
+} //fin de la clase iconos
